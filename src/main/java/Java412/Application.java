36ab7ff4f0b8f7167fed7fb5e10b83a28aba81a1
Ke1en <Andpey-dok@mail.ru>
2025-06-09 21:38:38 +0600

    Реализовать свой ArrayList<> (Методы - добавить элемент, добавить элемент по индексу, получить элемент, удалить элемент, очистить всю коллекцию, отсортировать) остальное по желанию.

---
+++ src/main/java/Java412/Application.java
@@ -0,0 +1,32 @@
+package Java412;
+
+import java.util.Comparator;
+
+public class Application {
+    public static void main(String[] args) {
+
+        MyArrayList<Integer> listInt = new MyArrayList<>();
+        listInt.add(4);
+        listInt.add(17);
+        listInt.add(8);
+        listInt.add(10);
+
+        listInt.remove(3);
+
+        listInt.insert(1, 25);
+
+        listInt.sort(Comparator.naturalOrder());
+
+        for (int i = 0; i < listInt.size(); i++) {
+            System.out.println(listInt.get(i));
+        }
+
+        listInt.clear();
+
+        for (int i = 0; i < listInt.size(); i++) {
+            System.out.println(listInt.get(i));
+        }
+
+    }
+
+}
---
+++ src/main/java/Java412/MyArrayList.java
@@ -0,0 +1,136 @@
+package Java412;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class MyArrayList<T> {
+    private T[] array;
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final int MULTIPLIER = 2;
+    private int pointer = 0;
+
+    public MyArrayList() {
+        this.array = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyArrayList(int capacity) {
+
+        if (capacity <= 0)
+            throw new IllegalArgumentException("Capacity must be greater than 0");
+
+        this.array = (T[]) new Object[capacity];
+
+    }
+
+    public void add(T element) {
+
+        if (pointer >= array.length)
+            resize();
+
+        array[pointer] = element;
+        pointer++;
+
+    }
+
+    public void insert(int index, T element) {
+
+        if (index == pointer) {
+            add(element);
+
+            return;
+        }
+
+        checkIndexBounds(index);
+        if (pointer + 1 >= array.length)
+            resize();
+
+        System.arraycopy(array, index, array, index + 1, pointer - 1);
+        array[index] = element;
+        pointer++;
+
+    }
+
+    public T get(int index) {
+        return array[index];
+    }
+
+    public T remove(int index) {
+
+        checkIndexBounds(index);
+        T element = array[index];
+        System.arraycopy(array, index + 1, array, index, pointer - index - 1);
+        pointer--;
+        array[pointer] = null;
+
+        return element;
+
+    }
+
+    public void clear() {
+        Arrays.fill(array, null);
+        pointer = 0;
+    }
+
+    public void sort(Comparator<? super T> comparator) {
+        quickSort(0, pointer - 1, comparator);
+    }
+
+    private void quickSort(int leftArrayBound, int rightArrayBound, Comparator comparator) {
+
+        if (leftArrayBound >= rightArrayBound)
+            return;
+
+
+        int middle = leftArrayBound + ((rightArrayBound - leftArrayBound) / 2);
+        T pivot = array[middle];
+
+        int leftBound = leftArrayBound;
+        int rightBound = rightArrayBound;
+
+        while (leftBound <= rightBound) {
+            while (comparator.compare(array[leftBound], pivot) < 0) {
+                leftBound++;
+            }
+
+            while (comparator.compare(array[rightBound], pivot) > 0) {
+                rightBound--;
+            }
+
+            if (leftBound <= rightBound) {
+                T temp = array[leftBound];
+                array[leftBound] = array[rightBound];
+                array[rightBound] = temp;
+                leftBound++;
+                rightBound--;
+            }
+
+        }
+
+        if (leftArrayBound < rightBound) {
+            quickSort(leftArrayBound, rightBound, comparator);
+        }
+
+        if (rightArrayBound > leftBound) {
+            quickSort(rightBound, leftArrayBound, comparator);
+        }
+
+    }
+
+    public int size() {
+        return pointer;
+    }
+
+    private void checkIndexBounds(int index) {
+        if (index < 0 || index >= pointer)
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + pointer);
+    }
+
+    private void resize() {
+
+        T[] newArray = (T[]) new Object[array.length * MULTIPLIER];
+        System.arraycopy(array, 0, newArray, 0, array.length);
+        array = newArray;
+
+    }
+
+}
